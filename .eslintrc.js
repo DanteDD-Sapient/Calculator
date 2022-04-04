712278ded1a2f637ae9fe34cfb4d8fa6f722894a
DanteDD-Sapient <dandanie2@publicisgroupe.net>
2022-04-04 17:59:22 +1000

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb-typescript',
+        'airbnb',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        ],
+    },
+}
