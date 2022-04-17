60fafe817cdcf913dbbc83ec67f94dee12c4deb4
DanteDD-Sapient <dandanie2@publicisgroupe.net>
2022-04-17 16:22:11 +1000

    style: added hover styles to button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,15 +3,16 @@ import React from 'react'
 export interface Props {
     classes: string
     handler: () => void
+    children: React.ReactChild
 }
-export default function Button({ classes, handler }: Props) {
+export default function Button({ classes, handler, children }: Props) {
     return (
         <button
             onClick={handler}
-            className={`p-4 text-white ${classes}`}
+            className={`p-4 text-white cursor-pointer hover:opacity-90 ${classes}`}
             type="button"
         >
-            button
+            {children}
         </button>
     )
 }
